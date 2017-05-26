f29afed547626f65e623f682ce8f2e783d853c67
Edmundo Elizondo <edmundo.geg@hotmail.com>
2017-05-26 04:20:42 +0000

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,31 @@
+class Author
+
+  @@post_count = 0
+
+  def self.post_count
+    @@post_count
+  end
+
+
+  attr_accessor :name
+  attr_reader :posts
+
+  def initialize(name)
+    @name = name
+    @posts = []
+  end
+
+  def add_post(post)
+    @posts << post
+    post.author = self
+    @@post_count += 1
+  end
+
+  def add_post_by_title(title)
+    post = Post.new(title)
+    @posts << post
+    post.author = self
+    @@post_count += 1
+  end
+
+end
