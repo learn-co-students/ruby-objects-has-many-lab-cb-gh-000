599cc8fd91d3bdb359b9117cad6d5a3e53f0bd5f
Pranav Sharma <ps20maths@gmail.com>
2018-07-06 17:18:16 +0000

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,13 @@
+class Post
+  attr_accessor :title, :author
+  @@all = []
+  def initialize(title)
+    @title = title
+    @@all << self
+  end
+
+  def author_name
+    @author != nil ? @author.name : nil
+  end
+
+end
