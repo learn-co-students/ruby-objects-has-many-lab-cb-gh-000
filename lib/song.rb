787cdfe1ba3052c379d696558269112b467c198b
Luisbert Seijas <luisbert2506@gmail.com>
2019-09-03 10:09:44 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,22 @@
+class Song
+  attr_accessor :name, :artist
+  
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    save
+  end
+
+  def artist_name
+    self.artist == nil ? nil : self.artist.name
+  end
+  
+  def save
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+end
